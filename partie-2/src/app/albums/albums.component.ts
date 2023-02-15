08ade31a4ad59337d3f3aaa39a6b92de46e79770
rickaed <cedric.blesch@sncf.fr>
2023-02-15 17:04:40 +0100

    try album

--- partie-2/src/app/albums/albums.component.ts
+++ partie-2/src/app/albums/albums.component.ts
@@ -18,17 +18,23 @@ export class AlbumsComponent implements OnInit {
 
   ngOnInit() {
     this.routeSub = this.route.params.subscribe(params => {
-      console.log(params) //log the entire params object
+      // console.log(params) //log the entire params object
       console.log(params['id']) //log the value of id
       this.artistId = params['id']
     });
-    return this.http.get<Album[]>(`http://localhost:4080/api/artists/""/albums`).subscribe(data => {
+    return this.http.get<Album[]>(`http://localhost:4080/api/artists/${this.artistId}/albums`).subscribe(data => {
       console.log('response :', data);
-      this.albums=data.filter((ele: Album) => {
-        ele.artists[0].id=this.artistId
+      data.forEach((album:Album )=> {
+        if (album.artists[0].id === this.artistId)
+        console.log(album.name);
+        
+        this.albums.push(album)
       });
       console.log(this.albums)
-    });
+        // album.artists[0].id=this.artistId
+      });
+      // console.log(this.albums)
+    };
   }
 
-}
+
